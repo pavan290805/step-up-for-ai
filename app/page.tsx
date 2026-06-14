0fce4570a6abdf49828c533dafb7ba5a0a415514
Aditya joshi <168006690+Aditya44234@users.noreply.github.com>
2026-06-15 00:06:00 +0530

    Basic commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-
-  return(
-    <div className="">First code for this step up backend . </div>
-  )
-
-
-
+  return <div className="">First code for this step up AI backend . </div>;
 }
